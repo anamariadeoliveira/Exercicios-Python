1b424e7cae0d662082224f86906a5f8847e467ea
Ana Maria de Oliveira <117085967+anacruvinelgo@users.noreply.github.com>
2023-01-25 16:04:20 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,2 +1,4 @@
+#Criando um script que leia o nome da pessoa e mostre uma mensagem de boas-vindas de acordo com o nome digitado
+
 nome = input('Qual é o seu nome? ')
 print('Bem vindo(a), ' + nome)
