55582b1187c18e4f5e6880cf36412dd22fe059ee
Ana Maria de Oliveira <117085967+anacruvinelgo@users.noreply.github.com>
2023-01-25 16:25:53 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,4 +1,4 @@
-#Criando um script eu leia dois números e mostre a soma entre eles.
+#Lendo dois números e mostrando a soma entre eles.
 
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
