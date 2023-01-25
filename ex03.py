f6df69bd50516c99a69bb658f73d309c6f8028cc
anacruvinelgo <anamaria_cgomes@hotmail.com>
2023-01-25 16:22:41 -0300

    exercicios de Python.

---
+++ ex03.py
@@ -0,0 +1,8 @@
+n1 = int(input('Por favor digite um número: '))
+n2 = int(input('Por favor digite outro número: '))
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n1 == n2:
+    print('Os valores são iguais')
+else:
+    print('O segundo valor é maior')
