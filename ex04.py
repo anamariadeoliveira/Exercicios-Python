eaf85849101bc9f5e7b6e8e2484ad197e11ae6df
Ana Maria de Oliveira <117085967+anacruvinelgo@users.noreply.github.com>
2023-02-16 13:14:43 -0300

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,3 +1,5 @@
+#Programa que mostre na tela uma contagem regressiva, indo de 10 até 0 com pausa de 1 segundo entre eles.
+
 from time import sleep
 print('Contagem Regressiva')
 for cont in range(10, -1, -1):
