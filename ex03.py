c6b9472ba05e1c150fcf2ff6ff416c7f436c6871
Ana Maria de Oliveira <117085967+anacruvinelgo@users.noreply.github.com>
2023-02-16 13:15:22 -0300

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,4 +1,5 @@
 #Escrevendo um programa que leia dois números inteiros e comparando-os, mostrando na tela as mensagens:O primeiro valor é maior, o segundo valor é maior e os valores são iguais.
+
 n1 = int(input('Por favor digite um número: '))
 n2 = int(input('Por favor digite outro número: '))
 if n1 > n2:
